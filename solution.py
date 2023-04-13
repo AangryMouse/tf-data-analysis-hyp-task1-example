7a43a98e27bcb5244c25c43861a2cbbc46f8fd6c
AangryMouse <71213909+AangryMouse@users.noreply.github.com>
2023-04-13 16:01:14 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 396317433
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+  
+    prop_control = x_success / x_cnt
+    prop_test = y_success / y_cnt
+
+    z_stat, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+
+    return z_stat > 1.555
